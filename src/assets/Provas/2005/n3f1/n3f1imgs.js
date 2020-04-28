72e98b0b4e3ac7c8a2161233766aaf2135afed2b
GuilhermeGabriel <guilhermegabriel90@gmail.com>
2020-04-27 22:34:07 -0300

    added n2f12005

---
+++ src/assets/Provas/2005/n3f1/n3f1imgs.js
@@ -0,0 +1,18 @@
+export const Images = {
+    r0: require('./n3f1imgs/q1.png'),
+    r2: require('./n3f1imgs/q3.png'),
+    r3: require('./n3f1imgs/q4.png'),
+    r4: require('./n3f1imgs/q5.png'),
+    r6: require('./n3f1imgs/q7.png'),
+    r7: require('./n3f1imgs/q8.png'),
+    r9: require('./n3f1imgs/q10.png'),
+    r11: require('./n3f1imgs/q12.png'),
+    r13: require('./n3f1imgs/q14.png'),
+    r14: require('./n3f1imgs/q15.png'),
+    r16: require('./n3f1imgs/q17.png'),
+    r17: require('./n3f1imgs/q18.png'),
+
+    r11: require('./n3f1imgs/r12.png'),
+    r17: require('./n3f1imgs/r18.png'),
+    r19: require('./n3f1imgs/r20.png'),
+}
